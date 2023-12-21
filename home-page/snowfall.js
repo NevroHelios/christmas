901c225610fc3f3840b8df2b11051855baba2bfa
NevroHelios <dasharka05@gmail.com>
2023-12-21 18:38:18 +0530

    updates

--- home-page/snowfall.js
+++ home-page/snowfall.js
@@ -1,17 +1,29 @@
-// Snowfall effect code
-const createSnowflake = () => {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-    snowflake.style.left = `${Math.random() * window.innerWidth}px`;
-    snowflake.style.top = '-5px'; // Start snowflakes above the viewport
-    const duration = 7 // Adjust duration for consistent falling speed
-    snowflake.style.animation = `snowfall ${duration}s linear infinite`;
-    document.body.appendChild(snowflake);
-  
-    setTimeout(() => {
-      snowflake.remove();
-    }, duration * 1000); // Adjust snowflake lifespan if needed
-  };
-  
-  setInterval(createSnowflake, 500); // Adjust interval for snowflake generation
-  
+// Function to generate a random number within a range
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// Function to create snowballs falling animation
+function createSnowball() {
+  const snowball = document.createElement('div');
+  snowball.classList.add('snowball');
+  snowball.style.left = `${getRandom(0, window.innerWidth)}px`;
+  document.getElementById('snowfall-container').appendChild(snowball);
+
+  // Animate the snowball falling and decreasing opacity
+  const animationDuration = getRandom(4, 8); // Adjust falling speed
+  snowball.style.animation = `fall ${animationDuration}s linear forwards, fade 1s ${animationDuration - 1}s forwards`;
+
+  // Remove snowball element when it reaches the bottom
+  snowball.addEventListener('animationend', () => {
+    snowball.remove();
+  });
+}
+
+// Create multiple snowballs falling at random intervals
+function generateSnowballs() {
+  setInterval(createSnowball, 1000); // Adjust snowfall frequency
+}
+
+// Run the function when the content is loaded
+window.addEventListener('load', generateSnowballs);
--- morphy2.js
+++
@@ -1,5 +0,0 @@
-// Using GSAP's MorphSVG plugin
-document.getElementById('explore-btn').addEventListener('click', function() {
-    gsap.to(".home", {duration: 1, morphSVG:".main-content"});
-  });
-  
