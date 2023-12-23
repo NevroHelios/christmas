383a2cafc509de20f5388ab3bd63fedbf523beb5
sayan chatterjee <chatterjeesayan1009@gmail.com>
2023-12-23 18:01:09 +0530

    valorant

--- home-page/game/MINI VALORANT/minivalorangegame.js
+++ home-page/game/MINI VALORANT/minivalorangegame.js
@@ -1,16 +1,30 @@
-let playbox=document.querySelector("#playbox2");
-
+let playbox=document.querySelector(".playbox2");
+let body=document.querySelector("body");
 let scoreboard=document.querySelector("h1");
 
 let score=0;
+let flag=1;
+let present=0;
 let starttimeout=prompt("enter starting timeout in miliseconds");
 let decrease=prompt("enter decrease in timeout in miliseconds");
-playbox.addEventListener("dblclick", range);
+playbox.addEventListener("click", range);
 
 function range(){
+    if(flag==1)
+    playbox.remove();
+    if(body.hasChildNodes==false){
+        playbox=document.createElement("button");
+        playbox.innerText="Game Over!! Refresh the game !!";
+        playbox.classList.add("playbox2");
+    }
+    /* let score=0;
+    let starttimeout=prompt("enter starting timeout in miliseconds");
+    let decrease=prompt("enter decrease in timeout in miliseconds");
+    scoreboard.innerText="Score:"+score; */
     score=score+1;
     let side=Math.floor(Math.random()*100)+25;
     let smallbox=document.createElement("div");
+    present++;
     smallbox.style.height=side+"px";
     smallbox.style.width=side+"px";
     let postop=Math.floor(Math.random()*500)+100;
@@ -25,11 +39,13 @@ function range(){
     smallbox.style.borderRadius="50%";
     smallbox.style.backgroundImage="url('giftimage.jpg')";
     smallbox.style.backgroundSize="cover";
-    playbox.append(smallbox);
+    body.append(smallbox);
     setTimeout(()=>{
         smallbox.remove();
         scoreboard.innerText="Score:"+score;
     }, starttimeout);
     starttimeout=starttimeout-decrease;
+    smallbox.addEventListener("click", ()=>{
+        smallbox.remove();});
+    smallbox.addEventListener("click", range);
 }
-
